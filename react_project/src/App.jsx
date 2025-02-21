13a3479419f2d1806b5c467fe7d4e7c83c1d9d95
chiaconso <chiaconso97@gmail.com>
2025-02-21 12:04:53 +0100

    feat: add class hello world

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+class HelloWorld extends Component {
+  render () {
+    return (
+      <h1>Hello World!</h1>
+    ) 
+  }
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <HelloWorld />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
