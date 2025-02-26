932b169eefc9ea65663cadc9500f8d7c58ac1c7d
chiaconso <chiaconso97@gmail.com>
2025-02-26 12:09:35 +0100

    feat: add increment counter

--- react_project/src/Counter.jsx
+++ react_project/src/Counter.jsx
@@ -2,9 +2,16 @@ import { useState } from "react"
 
 function Counter() {
     const [count, setCount] = useState(0)
+    function Increment() {
+        setCount(count=> {
+         return count +1
+        })
+    }
     return(<div>
+        <button onClick={Increment}>Increment</button>
         Count: {count}
-    </div>)
+    </div>
+    )
 } 
 
 export default Counter
