a4914dd59ad1ea750507348a65fcce3c1def60ba
chiaconso <chiaconso97@gmail.com>
2025-03-14 19:03:53 +0100

    feat: add navigation

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';  // Importa Link da react-router-dom
 import Home from './pages/Home';
 import About from './pages/About';
 
 const App = () => {
   return (
     <div>
+      {/* Aggiungi la barra di navigazione */}
       <nav>
         <ul>
           <li>
+            {/* Collegamento per la Home */}
             <Link to="/">Home</Link>
           </li>
           <li>
+            {/* Collegamento per la pagina About */}
             <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
 
+      {/* Definisci le rotte */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
