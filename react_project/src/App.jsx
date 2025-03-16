e224af3ebc1c74b09f33aac21aaf98e1981a32d5
chiaconso <chiaconso97@gmail.com>
2025-03-16 11:50:47 +0100

    feat: add layout in app

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';  
+import Layout from './components/Layout'; 
 import Home from './pages/Home';
 import About from './pages/About';
 
