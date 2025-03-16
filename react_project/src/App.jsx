69beb0d18bbd76acc285d45eb5ea1fa20b321de5
chiaconso <chiaconso97@gmail.com>
2025-03-16 11:41:10 +0100

    feat: add outlet

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';  // Importa Link da react-router-dom
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';  
 import Home from './pages/Home';
 import About from './pages/About';
 
 const App = () => {
   return (
-    <div>
-      {/* Aggiungi la barra di navigazione */}
-      <nav>
-        <ul>
-          <li>
-            {/* Collegamento per la Home */}
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            {/* Collegamento per la pagina About */}
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* Definisci le rotte */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ react_project/src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
